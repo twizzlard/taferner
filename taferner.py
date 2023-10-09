384809e261fa5fb0c5035243c20301f7a3dd4253
twizzlard <76930650+twizzlard@users.noreply.github.com>
2023-10-09 19:35:17 -0400

    Update taferner.py

--- taferner.py
+++ taferner.py
@@ -5,12 +5,76 @@ import re
 import base64
 import subprocess
 
-# Function to install 'openpyxl'
-def install_openpyxl():
+# Install 'openpyxl' immediately when the app starts
+try:
+    import openpyxl
+except ImportError:
+    st.warning("Installing 'openpyxl'. Please wait...")
     result = subprocess.run(['pip', 'install', 'openpyxl'], capture_output=True, text=True)
-    return result
+    if result.returncode == 0:
+        st.success("Successfully installed 'openpyxl'!")
+    else:
+        st.error("Failed to install 'openpyxl'. Check the error message below:")
+        st.code(result.stderr)
+        st.stop()
 
-# ... (rest of your code)
+# Create a function to find the matching category
+def find_matching_category(category, category_list):
+    category_lower = category.lower().replace(' ','')
+    for cat in category_list:
+        if category_lower == cat.lower():
+            return cat
+        elif category_lower.startswith(cat.lower() + ">"):
+            return cat
+        else:
+            # Check for partial string matching
+            parts = cat.lower().split(" > ")
+            for i in range(len(parts) - 1, 0, -1):
+                partial_cat = " > ".join(parts[:i])
+                if category_lower.startswith(partial_cat):
+                    return partial_cat
+    return "No match"
+
+# Function to process and save data
+def process_data(uploaded_file):
+    products = pd.read_excel(uploaded_file, sheet_name='products')
+    shipping = pd.read_excel(uploaded_file, sheet_name='shipping')
+    shipping['Category'] = shipping['Category'].str.lower().str.replace(' ','')
+    shipping = shipping.drop_duplicates()
+
+    categoryList = shipping['Category'].tolist()
+    categoryList = [x.replace(' ','').lower() for x in categoryList]
+    categoryList = sorted(list(set(categoryList)))
+
+    products['Categories'] = products['Categories'].ffill()
+
+    # Create products['Matching_Category'] for matching purposes and find the match category for each row
+    products['Matching_Category'] = products['Categories'].apply(lambda x: find_matching_category(x, categoryList))
+
+    # Merge the products table with the shipping data
+    products = products.merge(shipping, how='left', left_on='Matching_Category', right_on='Category')
+
+    # Change 'Published' to '-1' where 'Weight' is 'draft'
+    products.loc[products['Weight'] == 'draft', 'Published'] = "'-1"
+
+    # Drop the old dimension products columns and rename the new ones to the old names
+    products = products.drop(columns=['Category', 'Weight (kg)', 'Length (cm)', 'Width (cm)', 'Height (cm)'])
+    products = products.rename(columns={'Weight':'Weight (kg)', 'Length':'Length (cm)', 'Width':'Width (cm)', 'Height':'Height (cm)'})
+
+    # Save URLs as plaintext so they don't get cut off by Excel limitations
+    products['Images'] = products['Images'].astype(str)
+
+    # Create an ExcelWriter object with specific options
+    excel_writer = pd.ExcelWriter('Final_Excel.xlsx', engine='xlsxwriter')
+    excel_writer.book.strings_to_urls = False  # Disable automatic conversion of strings to URLs
+
+    # Write the DataFrame to Excel
+    products.to_excel(excel_writer, index=False, sheet_name='Sheet1')
+
+    # Save the Excel file
+    excel_writer.save()
+
+    return 'Final_Excel.xlsx'
 
 # Streamlit UI
 def main():
@@ -19,20 +83,6 @@ def main():
 
     # File uploader for Excel file
     uploaded_file = st.file_uploader("Upload an Excel file", type=["xls", "xlsx"])
-
-    # Install 'openpyxl' if not already installed
-    try:
-        import openpyxl
-    except ImportError:
-        st.warning("The 'openpyxl' library is not installed. Click below to install it.")
-        if st.button("Install openpyxl"):
-            installation_result = install_openpyxl()
-            if installation_result.returncode == 0:
-                st.success("Successfully installed 'openpyxl'! You may now upload and process Excel files.")
-            else:
-                st.error("Failed to install 'openpyxl'. Check the error message below:")
-                st.code(installation_result.stderr)
-        return
 
     if uploaded_file is not None:
         st.write(f"Uploaded file: {uploaded_file.name}")
